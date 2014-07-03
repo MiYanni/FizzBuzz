2ab1de6473ceca02ae02ee929e266096d27955e9
Michael Yanni <michael_yanni@bradycorp.com>
2014-07-03 12:49:38 -0400

    Added a Lookup table problem to the project. It was a simple linq expression, but I needed to hash it out.

---
+++ FizzBuzz/LookupTableProblem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    //public static class LookupTableProblem
+    //{
+    //    public static void Solution()
+    //    {
+    //        string[] testStrings = {"cat", "house", "bacon", "salad"};
+    //        LookupItem[] testItems =
+    //        {
+    //            new LookupItem {ID = 1, Name = "salad"},
+    //            new LookupItem {ID = 2, Name = "bacon"},
+    //            new LookupItem {ID = 3, Name = "cat"},
+    //            new LookupItem {ID = 4, Name = "house"}
+    //        };
+    //        //var sameStrings = testStrings.Intersect(testItems.Select((ti => ti.Name)));
+    //        //testItems.Where(ti => testStrings.Contains(ti.Name))
+    //        var iterateThroughThis = testStrings.Select(ts => testItems.First(ti => ti.Name == ts));
+    //    }
+    //}
+
+    //public class LookupItem
+    //{
+    //    public int ID { get; set; }
+    //    public string Name { get; set; }
+    //}
+
+    //// http://brendan.enrick.com/post/linq-your-collections-with-iequalitycomparer-and-lambda-expressions.aspx
+    //public class LambdaComparer<T> : IEqualityComparer<T>
+    //{
+    //    private readonly Func<T, T, bool> _lambdaComparer;
+    //    private readonly Func<T, int> _lambdaHash;
+
+    //    public LambdaComparer(Func<T, T, bool> lambdaComparer) :
+    //        this(lambdaComparer, o => 0)
+    //    {
+    //    }
+
+    //    public LambdaComparer(Func<T, T, bool> lambdaComparer, Func<T, int> lambdaHash)
+    //    {
+    //        if (lambdaComparer == null)
+    //            throw new ArgumentNullException("lambdaComparer");
+    //        if (lambdaHash == null)
+    //            throw new ArgumentNullException("lambdaHash");
+
+    //        _lambdaComparer = lambdaComparer;
+    //        _lambdaHash = lambdaHash;
+    //    }
+
+    //    public bool Equals(T x, T y)
+    //    {
+    //        return _lambdaComparer(x, y);
+    //    }
+
+    //    public int GetHashCode(T obj)
+    //    {
+    //        return _lambdaHash(obj);
+    //    }
+    //}
+}
