a7d85e6132e67592144c1d0cd8a6a01905f6429d
Michael Yanni <miyanni@microsoft.com>
2017-02-28 14:31:11 -0800

    Removed non-FizzBuzz related code.

--- FizzBuzz/ObjectOriented/FizzBuzzElement4.cs
+++ FizzBuzz/ObjectOriented/FizzBuzzElement4.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms.VisualStyles;
+﻿using System.Globalization;
 
 namespace FizzBuzz.ObjectOriented
 {
